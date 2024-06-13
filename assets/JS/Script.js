b18742eee19aae8062a58c6dbcaa9f151a2044a2
IbrahimNourEldeen <149830460+IbrahimNourEldeen@users.noreply.github.com>
2024-06-13 15:12:05 +0300

    Add files via upload

---
+++ assets/JS/Script.js
@@ -0,0 +1,6 @@
+var typed=new Typed(".basicHeader",{
+    strings:["I'm glad to see you on my site.","this is Your Favorite Place for Free Bootstrap Themes"],
+    typeSpeed:150,
+    backSpeed:150,
+    looped:true
+})
